eb47812000394e81a694655c3f1ebc9879756317
Diego GC <127536327+Desmondgtx@users.noreply.github.com>
2026-01-18 23:53:44 -0300

    update_analisis

---
+++ analisis_datos.m
@@ -0,0 +1,15 @@
+
+% Análisis datos
+
+% 1) ver el BIC total de cada modelo
+% one_k_one_beta     one_k_two_beta     two_k_one_beta      two_k_two_beta    
+% 3879.58610951623	5216.93519856285	4054.3179600344	    5542.11566808483
+
+
+% 2) dividir la muestra en vulnerable y no vulnerable y sumar separadamente los BIC por grupo (o correr los modelos con cada muestra grupal por separado)
+
+
+% 3) pescar el modelo 2k1b y ver diferencia  k_other - k_self (por sujeto y ver la dispersión) 
+
+
+% 4) wilcoxon rank entre vulnerable y no vulnerable
